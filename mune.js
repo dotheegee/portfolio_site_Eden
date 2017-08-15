00b2828da099673cdb090fc14cdfb493c043fb93
Eden <dldrmak@naver.com>
2017-08-15 16:26:25 +0900

    코드 개선

--- mune.js
+++ mune.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#menu-icon").on("click",function(){
-                       $("header nav ul").toggleClass("open");
+    $("#menu_icon").on("click",function(){
+                       $("#header #nav ul").toggleClass("open");
                        }); 
 });
